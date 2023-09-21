d1a3a1f6f4613d9804a277385504bd1cb3be4fc8
Mubashir Angathil <mubashirangathil5142@gmail.com>
2023-09-22 03:07:33 +0530

    SignUp form creation

---
+++ src/components/sign-up-form/SignUpFormComponent.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Box, FormControl, TextField } from "@mui/material"
+import SubmitButtonComponent from '../buttons/SubmitButtonComponent'
+
+const SignUpFormComponent: React.FC = () => {
+    return (
+        <Box component='form' p={2} display='grid' gap={2}>
+
+            <FormControl fullWidth>
+                <TextField label='Username' placeholder='jhon@gmail.com' type='email' size='small' />
+            </FormControl>
+
+            <FormControl fullWidth>
+                <TextField label='Password' placeholder='password' type='password' size='small' />
+            </FormControl>
+
+            <FormControl fullWidth>
+                <TextField label='Confirm Password' placeholder='password' type='password' size='small' />
+            </FormControl>
+
+            <SubmitButtonComponent title='Create Account' sx={{ mt: 2 }} onClick={() => { }} />
+
+        </Box>
+    )
+}
+
+export default SignUpFormComponent
