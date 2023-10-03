d4153841d7c9441d23caa4832e760cfff29f661c
Mubashir Angathil <mubashirangathil5142@gmail.com>
2023-10-03 22:56:12 +0530

    Page projects creation

--- src/pages/home/project/PageProjects.tsx
+++ src/pages/home/project/PageProjects.tsx
@@ -1,7 +1,43 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Grid, Button } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import ProjectCardComponent from "../../../components/card/project-card/ProjectCardComponent";
+
 const PageProjects: React.FC = () => {
-  return <Box>Projects</Box>;
+  return (
+    <Grid container gap={2}>
+      <Grid item xs={12} display="flex" justifyContent="space-between">
+        <Button variant="contained" endIcon={<AddIcon />}>
+          Create Project
+        </Button>
+        <input type="search" />
+      </Grid>
+      <Grid xs={12}>
+        <ProjectCardComponent
+          projects={[
+            {
+              projectTitle: "ProjectName",
+              description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Pellentesque vel libero ut lacus rutrum viverra. Mauris
+              cursus, dui vitae vestibulum feugiat, velit metus ullamcorper
+              ex`,
+              numberOfIssues: 4,
+              status: "opened",
+            },
+            {
+              projectTitle: "ProjectName",
+              description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Pellentesque vel libero ut lacus rutrum viverra. Mauris
+              cursus, dui vitae vestibulum feugiat, velit metus ullamcorper
+              ex`,
+              numberOfIssues: 4,
+              status: "opened",
+            },
+          ]}
+        />
+      </Grid>
+    </Grid>
+  );
 };
 
 export default PageProjects;
