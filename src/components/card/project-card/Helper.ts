6a0bf1d0e6de7fac12d3d4cfa397723b6d803ba7
Mubashir Angathil <mubashirangathil5142@gmail.com>
2023-10-24 01:29:01 +0530

    Bugfix(project): Updated variable name of project response

--- src/components/card/project-card/Helper.ts
+++ src/components/card/project-card/Helper.ts
@@ -1,8 +1,8 @@
 // Importing the ProjectResponse type from the project services helper
-import { ProjectResponse } from "../../../services/project-services/Helper";
+import { ProjectsResponse } from "../../../services/project-services/Helper";
 
 // Defining the props interface for ProjectCardComponent
 export interface ProjectCardComponentProps {
-  projects: ProjectResponse["data"]; // Array of project data from the API response
+  projects: ProjectsResponse["data"]; // Array of project data from the API response
   handleProjectLoading: React.UIEventHandler<HTMLDivElement>; // Event handler for scrolling on project container
 }
