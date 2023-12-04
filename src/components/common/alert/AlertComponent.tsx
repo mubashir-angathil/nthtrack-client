9bd6402244c839ed49985e01544e2caada356c63
Mubashir Angathil <mubashirangathil5142@gmail.com>
2023-12-04 23:29:49 +0530

    Refactor(Alert): Enhance General Alert dialog

--- src/components/common/alert/AlertComponent.tsx
+++ src/components/common/alert/AlertComponent.tsx
@@ -1,20 +1,14 @@
-import { Box, Button, Divider, Typography, colors } from "@mui/material";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
 import { FC } from "react";
 import { useAlert } from "./Helper";
 
-const alert = {
-  width: "30vw",
-  height: 200,
-  position: "absolute",
-  top: 200,
-  right: "20vw",
-  transform: "translate(-50%,10%)",
-  backgroundColor: colors.grey[800],
-  borderRadius: 2,
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "space-between",
-};
 const AlertComponent: FC = () => {
   const {
     alert: {
@@ -25,47 +19,40 @@ const AlertComponent: FC = () => {
   } = useAlert();
 
   return (
-    <Box
-      component="div"
-      width="100vw"
-      height="100vh"
-      sx={{
-        position: "fixed",
-        top: 0,
-        zIndex: 1,
-        background: "rgba(0,0,0,0.5)",
-        display: open ? "block" : "none",
-      }}
+    <Dialog
+      open={open}
+      onClose={handleCloseAlert}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
     >
-      <Box sx={alert} component="div" className="alert">
-        <Box height={5}>
-          <Box
-            sx={{
-              height: 20,
-              m: 2,
-            }}
-          >
-            <Typography variant="h5">{title}</Typography>
-          </Box>
-          <Divider />
-        </Box>
-        <Box sx={{ m: 2 }}>
-          <Typography variant="h6">{message}</Typography>
-        </Box>
-        <Divider />
-        <Box sx={{}} display="flex" gap={2}>
-          <Button
-            onClick={() => {
-              handleCloseAlert();
-              response("reject");
-            }}
-          >
-            {negativeButton}
-          </Button>
-          <Button onClick={() => response("accept")}>{positiveButton}</Button>
-        </Box>
-      </Box>
-    </Box>
+      <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          {message}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button
+          onClick={() => {
+            handleCloseAlert();
+            response("reject");
+          }}
+          size="small"
+          variant="outlined"
+          color="error"
+        >
+          {negativeButton}
+        </Button>
+        <Button
+          variant="contained"
+          size="small"
+          onClick={() => response("accept")}
+          autoFocus
+        >
+          {positiveButton}
+        </Button>
+      </DialogActions>
+    </Dialog>
   );
 };
 
